5404c20ba25d4d5509ebee10210cd23629c7dd0f
condominio <pablowobido@msn.com>
2021-11-19 19:07:05 -0300

    primeiro

---
+++ app/model/pessoas/Cidade.php
@@ -0,0 +1,21 @@
+<?php
+
+    class Cidade extends TRecord
+    {
+        const TABLENAME = 'cidade';
+        const PRIMARYKEY = 'id';
+        const IDPOLUCY = 'max';
+
+        public function __construct($id = NULL, $callObjectLoad = TRUE)
+        {
+          parent::__construct($id, $callObjectLoad);
+          parent::addAttribute('nome');
+          parent::addAttribute('codigo_ibge');
+          parent::addAttribute('estado_id');
+        }
+
+        public function get_estado()
+        {
+            return Estado::find($this->estado_id);
+        }
+    } 
---
+++ app/model/pessoas/Estado.php
@@ -0,0 +1,15 @@
+<?php
+
+    class Estado extends TRecord
+    {
+        const TABLENAME = 'estado';
+        const PRIMARYKEY = 'id';
+        const IDPOLUCY = 'max';
+
+        public function __construct($id = NULL, $callObjectLoad = TRUE)
+        {
+          parent::__construct($id, $callObjectLoad);
+          parent::addAttribute('nome');
+          parent::addAttribute('uf');
+        }
+    } 
