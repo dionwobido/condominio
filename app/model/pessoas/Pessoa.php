10d0ca98c9d172ad3bf9822cb1c249f972f596f1
condominio <pablowobido@msn.com>
2021-11-19 21:46:49 -0300

    Alteracao teste5

---
+++ app/model/pessoas/Pessoa.php
@@ -0,0 +1,35 @@
+<?php
+
+    class Pessoa extends TRecord
+    {
+        const TABLENAME = 'pessoa';
+        const PRIMARYKEY = 'id';
+        const IDPOLUCY = 'max';
+
+        public function __construct($id = NULL, $callObjectLoad = TRUE)
+        {
+          parent::__construct($id, $callObjectLoad);
+          parent::addAttribute('nome');
+          parent::addAttribute('nome_fantasia');
+          parent::addAttribute('tipo');
+          parent::addAttribute('codigo_nacional');
+          parent::addAttribute('codigo_estadual');
+          parent::addAttribute('codigo_municipal');
+          parent::addAttribute('email');
+          parent::addAttribute('observacao');
+          parent::addAttribute('cep');
+          parent::addAttribute('logradouro');
+          parent::addAttribute('numero');
+          parent::addAttribute('complemento');
+          parent::addAttribute('bairro');
+          parent::addAttribute('cidade_id');
+          parent::addAttribute('grupo_id');
+          parent::addAttribute('create_at');
+          parent::addAttribute('update_at');
+        }
+
+        public function get_estado()
+        {
+            return Estado::find($this->estado_id);
+        }
+    } 
